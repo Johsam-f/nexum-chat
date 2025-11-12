bf8a1fe67de8ba380da79b8151c255cdad47a6f6
Johsam <johsamfcress@gmail.com>
2025-11-12 12:03:15 +0200

    feat: add Toaster component for notification display in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/components/ThemeProvider";
 import ThemeToggle from "@/components/ThemeToggle";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
             <div className="absolute top-4 right-4 z-50">
               <ThemeToggle />
             </div>
+            <Toaster position="top-center" />
             {children}
           </div>
         </ThemeProvider>
