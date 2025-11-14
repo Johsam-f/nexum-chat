aa730eeefae87b3a85ad7fbd0f075175c1453f2c
Johsam <johsamfcress@gmail.com>
2025-11-14 14:18:14 +0200

    feat:  include session validation and redirect to login

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,14 +1,23 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
+import { redirect } from "next/navigation"
+import { cookies } from "next/headers"
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <SidebarProvider className="flex min-h-screen">
-      <AppSidebar />
-      <main className="flex-1 mt-4">
-        <SidebarTrigger size="lg" />
-        {children}
-      </main>
-    </SidebarProvider>
-  )
+export default async function Layout({ children }: { children: React.ReactNode }) {
+    const cookieStore = await cookies()
+    const session = cookieStore.get('better-auth.session_token')
+    
+    if (!session) {
+        redirect('/login');
+    }
+    
+    return (
+        <SidebarProvider className="flex min-h-screen">
+            <AppSidebar />
+            <main className="flex-1 mt-4">
+            <SidebarTrigger size="lg" />
+            {children}
+            </main>
+        </SidebarProvider>
+    )
 }
